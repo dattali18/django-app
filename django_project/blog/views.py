3b1741dca1c4c269bf42ff8cd005c3eb1503e549
dattali18 <74492594+dattali18@users.noreply.github.com>
2023-12-25 12:37:44 +0200

    stopped at video #5 DataBase

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
-posts = [
-    {
-        'author': "Daniel Attali",
-        "title": "blog post 1",
-        "content": "blog post",
-        "date_posted": "Aug 27, 2023"
-    },
-    {
-        'author': "Daniel",
-        "title": "blog post",
-        "content": "blog",
-        "date_posted": "Feb 2, 2024"
-    }
-]
-
 
 def home(request):
     context = {
         'title': 'blog home',
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
