79a6c70e164f33c72d77c00a9c6a4e32286f95db
dattali18 <74492594+dattali18@users.noreply.github.com>
2023-12-26 12:42:52 +0200

    stopped at video #6

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom app
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    # imported app
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
