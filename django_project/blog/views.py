99a406ca88360322ce882934c0192a88a4ea6ea2
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-01-01 13:17:52 +0200

    stopped at video #1q

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.contrib.auth.models import User
 from .models import Post
 
 
@@ -24,6 +24,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<mode>_<view_type>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<mode>_<view_type>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
